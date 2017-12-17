e19b0571694f19e68ee715555535460473c9c864
John Tungul <jtungul53@yahoo.com>
2017-12-17 00:15:20 -0800

    Create round db table

--- BirdieScore/settings.py
+++ BirdieScore/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'round.apps.RoundConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- round/models.py
+++ round/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Round(models.Model):
+    round_id = models.IntegerField(primary_key=True)
+    course = None
