9f5a5a50cc1053ebd46c6d0776e4e1c7452dc3dd
John Tungul <jtungul53@yahoo.com>
2017-12-17 21:18:59 -0800

    add a couple of tests in round app

--- round/tests.py
+++ round/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Round
 
 # Create your tests here.
+class RoundModelTests(TestCase):
+
+    def test_create_round(self):
+        """If a round is created, it should be in the database"""
+        round = Round()
+        round.save()
+        self.assertIs(Round.objects.all().count(), 1)
+
+    def test_get_round(self):
+        """If an entry in the database is queried, should return true if it exists"""
+        round = Round()
+        round.save()
+        created_id = round.round_id
+        entry = Round.objects.get(round_id=created_id)
+        self.assertIs(Round.objects.filter(round_id=entry.round_id).exists(), True)
+
+    def test_get_round_not_in_db(self):
+        """If an entry in the database is queried, should return false if it does not exist"""
+        round = Round()
+        round.save()
+        created_id = round.round_id
+        entry = Round.objects.get(round_id=created_id)
+        self.assertIs(Round.objects.filter(round_id=-1).exists(), False)
+
