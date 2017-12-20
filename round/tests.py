cd9607abc5e226f61ff071408ec0339cdd337729
John Tungul <jtungul53@yahoo.com>
2017-12-19 17:39:57 -0800

    Add more tests to round query

--- round/tests.py
+++ round/tests.py
@@ -15,14 +15,20 @@ class RoundModelTests(TestCase):
         round = Round()
         round.save()
         created_id = round.round_id
-        entry = Round.objects.get(round_id=created_id)
-        self.assertIs(Round.objects.filter(round_id=entry.round_id).exists(), True)
+        #entry = Round.objects.get(round_id=created_id)
+        self.assertIs(Round.objects.filter(round_id=round.round_id).exists(), True)
 
     def test_get_round_not_in_db(self):
         """If an entry in the database is queried, should return false if it does not exist"""
         round = Round()
         round.save()
         created_id = round.round_id
-        entry = Round.objects.get(round_id=created_id)
+        #entry = Round.objects.get(round_id=created_id)
         self.assertIs(Round.objects.filter(round_id=-1).exists(), False)
+
+    def test_round_specific_id(self):
+        round = Round()
+        round.round_id = 1234
+        round.save()
+        self.assertIs(Round.objects.filter(round_id=1234).exists(), True)
 
