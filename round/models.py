60a2af88fc0f979c082595739ba8963c26f5680f
John Tungul <jtungul53@yahoo.com>
2017-12-19 21:38:02 -0800

    Handle cases when randomly generated integer already exists in db

--- round/models.py
+++ round/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from random import randint
 
 # Create your models here.
+
+#figure out what to do when no more available 4 digits.
+#also optimize this because it is inefficient when db gets crowded.
 def random_4_digits():
-    return randint(1000, 9999)
+    random_number = randint(1000, 9999)
+    while True:
+        if not Round.objects.filter(round_id=random_number).exists():
+            return random_number
+        random_number = randint(1000, 9999)
 
 class Round(models.Model):
     round_id = models.IntegerField(primary_key=True, default=random_4_digits)
