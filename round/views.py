72d1b5cf18101b3df90a086e20b73ee4176059c7
John Tungul <jtungul53@yahoo.com>
2017-12-20 23:34:27 -0800

    Add test urls

--- round/views.py
+++ round/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Round
+from .models import Round, Course
 
 # Create your views here.
 
@@ -10,5 +10,47 @@ def index(request):
 #this will be called when a user creates a new round
 def create_round(request):
     round = Round()
+    round.course = Course.objects.get(course_name='Skywest')
     round.save()
-    return HttpResponse("Created")
+    return HttpResponse('Created')
+
+def get_round_by_id(request, id):
+    round = Round.objects.get(round_id=id)
+    return HttpResponse(getattr(round.course,'course_name'))
+
+def create_course_by_name(request, name):
+    #TODO
+    #figoure out how we will assign hole numbers from frontend
+    course = Course()
+    course.course_name = name
+    course.hole_1 = 5
+    course.hole_2 = 4
+    course.hole_3 = 4
+    course.hole_4 = 5
+    course.hole_5 = 3
+    course.hole_6 = 4
+    course.hole_7 = 4
+    course.hole_8 = 3
+    course.hole_9 = 4
+    course.hole_10 = 4
+    course.hole_11 = 4
+    course.hole_12 = 3
+    course.hole_13 = 4
+    course.hole_14 = 5
+    course.hole_15 = 4
+    course.hole_16 = 3
+    course.hole_17 = 5
+    course.hole_18 = 4
+
+    course.save()
+
+    return HttpResponse("CREATED")
+
+def get_course(request, name):
+    course = Course.objects.get(course_name=name)
+    info = []
+    info.append('Course_name: {}\n'.format(getattr(course, 'course_name')))
+    for x in range(1,19):
+        info.append('Hole {}: {}\n'.format(x, getattr(course, 'hole_{}'.format(x))))
+    return HttpResponse("".join(info))
+
