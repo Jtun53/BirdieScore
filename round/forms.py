1051895ca80ab2833ce842378c48fc53514dc8d2
John Tungul <jtungul53@yahoo.com>
2018-01-14 11:52:38 -0800

    Add some courses on front end form

--- round/forms.py
+++ round/forms.py
@@ -4,7 +4,7 @@ from django.utils.safestring import mark_safe
 class RoundForm(forms.Form):
     player_name = forms.CharField(label='Player ID', max_length=100)
     round_id = forms.IntegerField(label='Round ID', required=False)
-    course = forms.ChoiceField(choices=(('', ''), ('Skywest', 'Skywest'),), required=False)
+    course = forms.ChoiceField(choices=(('', ''), ('Skywest', 'Skywest'), ('Mariner\'s Point', 'Mariner\'s Point'), ('Fremont Park', 'Fremont Park')), required=False)
 
 class ScoreForm(forms.Form):
     hole_num = forms.ChoiceField(label="Hole Number", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10),
