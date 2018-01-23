72485e65ac57205f477c6e7485e42d3f8d5a58f2
John Tungul <jtungul53@yahoo.com>
2018-01-22 22:26:58 -0800

    Add city and state in choice field

--- round/forms.py
+++ round/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from .models import Course
 
 class RoundForm(forms.Form):
     player_name = forms.CharField(label='Player ID', max_length=100)
     round_id = forms.IntegerField(label='Round ID', required=False)
-    course = forms.ChoiceField(choices=(('', ''), ('Skywest', 'Skywest'), ('Mariner\'s Point', 'Mariner\'s Point'), ('Fremont Park', 'Fremont Park')), required=False)
+    #course = forms.ChoiceField(choices=(('', ''), ('Skywest', 'Skywest'), ('Mariner\'s Point', 'Mariner\'s Point'), ('Fremont Park', 'Fremont Park')), required=False)
+    course = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super(RoundForm, self).__init__(*args, **kwargs)
+        self.fields['course'].choices = [(x.course_name, "{} - {}, {}".format(x.course_name, x.course_city, x.course_state))
+                                         for x in Course.objects.all()]
 
 class ScoreForm(forms.Form):
     hole_num = forms.ChoiceField(label="Hole Number", choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10),
